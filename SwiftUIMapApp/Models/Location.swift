f4d69b9bef73e1879b95164f6f8b2fa81ce38606
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-06-13 11:22:08 +0530

    Created ViewModel and added Map to the project

---
+++ SwiftUIMapApp/Models/Location.swift
@@ -0,0 +1,23 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by Saksham Gupta on 13/06/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Identifiable {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    var id: String {
+        name + cityName
+    }
+}
+
---
+++ SwiftUIMapApp/Views/LocationsView.swift
@@ -0,0 +1,27 @@
+//
+//  LocationsView.swift
+//  SwiftUIMapApp
+//
+//  Created by Saksham Gupta on 13/06/24.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct LocationsView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    LocationsView()
+        .environmentObject(LocationsViewModel())
+}
